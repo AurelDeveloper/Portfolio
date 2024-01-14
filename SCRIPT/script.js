0968c270d0a6f1c86836fd43ddf146cda2fd10bd
AurelDeveloper <marlon.profils@gmail.com>
2024-01-14 15:04:06 +0100

    .

---
+++ SCRIPT/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.files-box').slick({
+        dots: false, /* Disable navigation dots */
+        infinite: false, /* Disable infinite loop */
+        speed: 300,
+        slidesToShow: 1, /* Show one slide at a time */
+        slidesToScroll: 1, /* Scroll one slide at a time */
+        adaptiveHeight: true,
+        centerMode: true, /* Enable center mode */
+        centerPadding: '60px', /* Padding on the sides of the center slide. Adjust as needed. */
+        autoplay: true, /* Enable autoplay */
+        autoplaySpeed: 2000, /* Delay between slides (in ms) */
+        arrows: false, /* Disable previous/next arrows */
+    });
+});
